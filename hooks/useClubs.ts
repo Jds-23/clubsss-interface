50f95c6c5cebbf7b62bbbb367833f2b1167f6800
Joydeep Singha <joydeepsingha68@gmail.com>
2022-09-21 03:36:31 +0530

    feat: freefor all integrated

--- hooks/useClubs.ts
+++ hooks/useClubs.ts
@@ -28,18 +28,29 @@ const fetcher = (query) => request(clubv1subgraph, query);
 export function useClubs(): ClubInterface[] | undefined {
   const { data } = useSWR(QUERY, fetcher, { refreshInterval: 7000 });
   return data?.clubs?.map((club: any) => {
-    const decodedData = ethers.utils.defaultAbiCoder.decode(
-      ["address", "string", "string"],
-      club.data
-    );
-
+    let decodedData;
+    if (club.type === "0" || club.type === "1")
+      decodedData = ethers.utils.defaultAbiCoder.decode(
+        ["address", "string", "string"],
+        club.data
+      );
+    else if (club.type === "2")
+      decodedData = ethers.utils.defaultAbiCoder.decode(
+        ["string", "string"],
+        club.data
+      );
+    else
+      decodedData = ethers.utils.defaultAbiCoder.decode(
+        ["uint256", "string", "string"],
+        club.data
+      );
     return {
       id: club.id,
       type: parseFloat(club.type),
       ideasCount: parseFloat(club.ideasCount),
       data: club.data,
-      clubName: decodedData[1],
-      metadata: decodedData[2],
+      clubName: decodedData[decodedData.length - 2],
+      metadata: decodedData[decodedData.length - 1],
     };
   });
 }
@@ -48,18 +59,30 @@ export function useClub(id: string): ClubInterface {
   return data?.clubs
     ?.filter((club: any) => club.id === id)
     ?.map((club: any) => {
-      const decodedData = ethers.utils.defaultAbiCoder.decode(
-        ["address", "string", "string"],
-        club.data
-      );
+      let decodedData;
+      if (club.type === "0" || club.type === "1")
+        decodedData = ethers.utils.defaultAbiCoder.decode(
+          ["address", "string", "string"],
+          club.data
+        );
+      else if (club.type === "2")
+        decodedData = ethers.utils.defaultAbiCoder.decode(
+          ["string", "string"],
+          club.data
+        );
+      else
+        decodedData = ethers.utils.defaultAbiCoder.decode(
+          ["uint256", "string", "string"],
+          club.data
+        );
 
       return {
         id: club.id,
         type: parseFloat(club.type),
         ideasCount: parseFloat(club.ideasCount),
         data: club.data,
-        clubName: decodedData[1],
-        metadata: decodedData[2],
+        clubName: decodedData[decodedData.length - 2],
+        metadata: decodedData[decodedData.length - 1],
       };
     })[0];
 }
